7101d40fe2c69a440da2de3241d7d1b614283ef1
trizzlenova <tdswayzee@gmail.com>
2023-08-09 11:30:28 -0700

    base routes and controllers: home + about

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
+const baseController = require('./base.controller');
 const employeeController = require('./employee.controller');
 const eventController = require('./event.controller');
 const prospectController = require('./prospect.controller');
 
 module.exports = {
+    baseController,
     employeeController,
     eventController,
     prospectController
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const employeeRoutes = require('./employee.route');
 const eventRoutes = require('./event.route');
 const prospectRoutes = require('./prospect.route');
+const homeRoutes = require('./base.route');
 
 const router = express.Router();
 
+
+router.use("/", homeRoutes);
 router.use("/employees", employeeRoutes);
 router.use("/events", eventRoutes);
 router.use("/prospects", prospectRoutes);
