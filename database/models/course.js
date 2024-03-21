7e516b2aabaaaaa2bdb153003b54f414bd5b5b33
EricFithian <ef2413@columbia.edu>
2024-03-21 02:39:46 -0400

    Updated course model

--- database/models/course.js
+++ database/models/course.js
@@ -1,49 +1,33 @@
 const mongoose = require('mongoose');
-// const schema = new mongoose.Schema
+const { INTEGER } = require('sequelize');
 
 const courseSchema = new mongoose.Schema({
-      name: {
-        type: String,
-        required: [true, "Please enter a name for the course"],
-        unique: [true, "Must be a unique course name"]
-      }, description: {
-        type: String,
-        required: [true, "Please enter a course description"]
-      }, startDate: {
-        type: Date,
-        required: [true, "please enter a start date"]
-      }, endDate: {
-        type: Date,
-        required: [true, "please enter an end date"]
-      }, hours: {
-        type: Number,
-        default: 40*13
-      }, isFullTime: {
-        type: Boolean,
-        default: true
-      }, courseType: {
-        type: String,
-        default: 'online'
-      }}, {
-        timestamps: true
-    }
+  firstName: {
+    type: String,
+  }, lastName: {
+    type: String,
+  }, 
+  description: {
+    type: String,
+    default: ""
+  }, startDate: {
+    type: Date,
+    default: Date.now()
+  }, endDate: {
+    type: Date,
+    default: Date.now() + (24 * 7 * 13)
+  }, hours: {
+    type: INTEGER,
+    default: 13 * 5 * 8
+  },
+  isHidden: {
+    type: Boolean,
+    default: false
+  }}, {
+    timestamps: true
+  }
 );
 
-// mongoose.model(<mongodb collection name>, our schema) is the general default and it creates a collection inside of MongoDB that is named from the first argument, Musicians here. And it applies the schema above to that collection.
-const Courses = mongoose.model('Course', courseSchema);
+const Courses = mongoose.model('Courses', courseSchema);
 
-module.exports = Courses;
-
-    // static associate(models) {
-    //   // define association here
-    //   Course.belongsToMany(models.Employee, {
-    //     through: models.Enrollment,
-    //     as: "employees",
-    //     foreignKey: "courseId"
-    //   });
-    //   Course.belongsToMany(models.Student, {
-    //     through: models.Enrollment,
-    //     as: "students",
-    //     foreignKey: "courseId"
-    //   });
-    // }
+module.exports = Courses;  
